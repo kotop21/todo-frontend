668f53b8eb20067d44f6e792c43829b35b0b39e7
Victor <72082776+kotop21@users.noreply.github.com>
2025-10-13 23:37:38 +0300

    Modify: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    // Разрешаем хост Render
+    allowedHosts: ['todo-frontend-bh64.onrender.com'],
+  },
   server: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4000,
